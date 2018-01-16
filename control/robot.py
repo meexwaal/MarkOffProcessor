90c10b5b23cf68677e0d02882533f70fb45d71db
max <maximusjohnsonus@gmail.com>
2018-01-15 19:45:55 -0500

    work on robot.py, still needs testing

--- control/robot.py
+++ control/robot.py
@@ -6,13 +6,13 @@ class robot:
 
     # Number of points from the path to consider for distance
     # (to keep the bot from getting confused)
-    self.numPathPts = 20
+    numPathPts = 20
 
     # PID to do line following
-    self.linePID = PID.PID()
+    linePID = PID.PID()
 
     # PID to do rotating to an angle
-    self.rotPID = PID.PID()
+    rotPID = PID.PID()
 
     class Mode(Enum):
         STILL = 0
@@ -23,12 +23,23 @@ class robot:
     # pos : (real,real) - the position of the bot on the grid
     # rot : (real,real) - the orientation of bot as XY accelerations
     # path : (real,real) array - the list of points on the path
-    # motor : (int,int) - the values to drive the motor
-    # mode : mode_enum - what the current mode should be
     # rotTarget : real - angle to rotate to
+    # mode : mode_enum - what the current mode should be
+    # lineSpeed : real - result of linePID, which is like angular speed
+    # angSpeed : real - result of rotPID, which is actually angular speed
+    # motor : (int,int) - the values to drive the motor
 
-    def __init__(self, pos = None, rot=None, path=None):
+    def __init__(self, pos = None, rot=None, path=None, rotTarget=None,
+                 mode=Mode.STILL):
         self.pos = pos
+        self.rot = rot
+        self.path = path
+        self.rotTarget = rotTarget
+        self.mode = mode
+
+        self.lineSpeed = 0
+        self.angSpeed = 0
+        self.motor = (0,0)
 
     def setPos(self, pos):
         self.pos = pos
@@ -39,27 +50,46 @@ class robot:
 
     # update should be called at a regular interval
     # (derivative should be wrt time, so needs a constant delta-T)
-    def update(self, pos, rot):
+    def update(self, pos, rot=None):
         self.pos = pos
-        self.rot = rot
-        if self.mode == Mode.LINE_FOLLOW:
-            self.linePID.update(self.getDistance())
-        elif self.mode == Mode.ROTATING:
-            self.rotPID.update(self.anglify(self.rot))
+        if rot != None:
+            self.rot = rot
+
+        if self.mode == self.Mode.LINE_FOLLOW:
+            self.lineSpeed = self.linePID.update(self.getDistance())
+        elif self.mode == self.Mode.ROTATING:
+            self.angSpeed = self.rotPID.update(self.anglify(self.rot))
 
     def changeMode(self, newMode):
         self.mode = newMode
-        if newMode == Mode.LINE_FOLLOW:
+        if newMode == self.Mode.LINE_FOLLOW:
             self.linePID.setPoint(0)
-        elif newMode == Mode.ROTATING:
+        elif newMode == self.Mode.ROTATING:
             self.rotPID.setPoint(self.rotTarget)
 
-    def followLine(self):
-        self.changeMode(Mode.LINE_FOLLOWING)
 
-    def rotateTo(self, angle):
-        self.rotTarget = angle
-        self.changeMode(Mode.ROTATING)
+    # Return (Lspeed,Rspeed) based on the current mode
+    def getMotors(self):
+        # TODO: make the numbers actually make sense
+        if self.mode == self.Mode.STILL:
+            return (0,0)
+        elif self.mode == self.Mode.ROTATING:
+            return (angSpeed, -angSpeed)
+        elif self.mode == self.Mode.LINE_FOLLOW:
+            return (100+self.lineSpeed, 100-self.lineSpeed) # or something like that
+
+    def followLine(self, path=None):
+        if path != None:
+            self.setPath(path)
+        self.changeMode(self.Mode.LINE_FOLLOW)
+        #
+    def rotateTo(self, angle=None):
+        if angle != None:
+            self.rotTarget = angle
+        self.changeMode(self.Mode.ROTATING)
+
+    def stop(self):
+        self.changeMode(self.Mode.STILL)
 
     # getDistance : void -> real
     def getDistance(self):
